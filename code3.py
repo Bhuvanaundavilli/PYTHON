5948189b052ed454e37d68f850529ec06d11ca93
Bhuvanaundavilli <156160693+Bhuvanaundavilli@users.noreply.github.com>
2024-02-22 10:18:27 +0530

    Delete code3.py

--- code3.py
+++
@@ -1,9 +0,0 @@
-Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a=int(input("Enter number of persons"))
-Enter number of persons 3
->>> b=int(input("Enter number of apples"))
-Enter number of apples 14
->>> remaining_apples=(b%a)
->>> print(remaining_apples)
-2
